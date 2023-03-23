cdbb026b503aa189176eaa063a359636e5ce0be9
ComposedNebula <81245965+ComposedNebula@users.noreply.github.com>
2023-03-23 13:55:05 -0400

    yur

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ root = tk.Tk()
 root.wm_geometry("400x700+1000+200")
 root.title("Rizzbook")
 
-
+# Welcome Page
 
 welcome_page = tk.Frame(root)
 welcome_page.grid(row=0, column=0, sticky="news")
 welcome_message = tk.Label(welcome_page, text="Rizzbook").place(anchor="center")
-# welcome_message.pack(padx=150)
+welcome_message.pack(padx=150)
 
 login_username = tk.Entry(welcome_page)
 login_username.pack()
@@ -23,4 +23,29 @@ login_forgot_password = tk.Button(welcome_page, text="Forgot Password", command=
 login_forgot_password.pack()
 login_create_account = tk.Button(welcome_page, text="Create new account", command=create_new_account)
 login_create_account.pack()
+
+
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+# Home Page
+
+home_page = tk.Frame(root)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 root.mainloop()
